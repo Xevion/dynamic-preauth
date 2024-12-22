b88296c41088ecc791b22bdfec3813020996d018
Xevion <xevion@xevion.dev>
2024-12-22 10:28:50 -0600

    improve display views, add random generation for demo version, improve badge flex, add ephemeral note

--- frontend/src/components/StatefulDemo.tsx
+++ frontend/src/components/StatefulDemo.tsx
@@ -12,14 +12,20 @@ type SessionData = {
 };
 
 const StatefulDemo = ({ class: className }: StatefulDemoProps) => {
+  const randomBits = (bits: number) =>
+    Math.floor(Math.random() * 2 ** bits)
+      .toString(16)
+      .padStart(bits / 4, "0")
+      .toUpperCase();
+
   const [session, setSession] = useState<SessionData | null>({
-    id: "0x59AF5BC09",
-    downloads: ["0xABF4", "0x1F3A", "0x2F3A"],
+    id: "0×" + randomBits(32),
+    downloads: Array.from({ length: 7 }).map(() => "0×" + randomBits(16)),
   });
 
   return (
     <div class={cn(className, "px-5 leading-6")}>
-      <p class="mt-3">
+      <p class="mt-3 mb-3">
         This demo uses websockets to communicate between the server and the
         browser. Each download gets a unique identifier bound to the user
         session.
@@ -33,9 +39,10 @@ const StatefulDemo = ({ class: className }: StatefulDemoProps) => {
           </>
         ) : null}
       </p>
-      <div>
+      <div class="flex flex-wrap justify-center gap-y-2.5">
         {session?.downloads.map((download) => (
           <Badge
+            className="grow-0"
             onClick={function onClick() {
               const audio = new Audio("/notify.wav");
               audio.volume = 0.3;
@@ -45,6 +52,13 @@ const StatefulDemo = ({ class: className }: StatefulDemoProps) => {
             {download}
           </Badge>
         ))}
+      </div>
+      <div class="mt-4 p-2 bg-zinc-900/90 rounded-md border border-zinc-700">
+        <p class="my-0">
+          The server running this is completely ephemeral, can restart at any
+          time, and purges data on regular intervals - at which point the
+          executables you've downloaded will no longer function.
+        </p>
       </div>
     </div>
   );
