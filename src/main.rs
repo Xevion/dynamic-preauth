1b81adcc86c4d8bde1a27891b092f31ff96f938b
Xevion <xevion@xevion.dev>
2024-12-21 22:41:32 -0600

    re-add PORT parsing

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ async fn hello() -> &'static str {
 
 #[tokio::main]
 async fn main() {
-    // let port = std::env::var("PORT").unwrap_or_else(|_| "5800".to_string());
-    let addr = format!("0.0.0.0:{}", "5800");
+    let port = std::env::var("PORT").unwrap_or_else(|_| "5800".to_string());
+    let addr = format!("0.0.0.0:{}", port);
     tracing_subscriber::fmt().init();
 
     let router = Router::new().get(hello);
