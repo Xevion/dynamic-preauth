9aaefc7bb419d011623718ea61b5a94806e12cb8
Xevion <xevion@xevion.dev>
2024-12-21 22:26:17 -0600

    Make demo use JSON, include compile time, sha256 hash, arg option

--- demo/build.rs
+++ demo/build.rs
@@ -1,3 +1,5 @@
+use serde::Serialize;
+use sha2::Digest;
 use std::{
     env,
     error::Error,
@@ -6,13 +8,32 @@ use std::{
     path::Path,
 };
 
+// Shared between build.rs and main.rs
+#[derive(Serialize, Debug)]
+struct KeyData<'a> {
+    value: &'a str,
+    // value_hash is not intended to be a secure trusted hash; I don't know if there's a way to ensure it stays unmodified regardless
+    value_hash: String,
+    compile_time: String,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = env::var("OUT_DIR")?;
-    let dest_path = Path::new(&out_dir).join("long_string.txt");
+    let dest_path = Path::new(&out_dir).join("key.json");
     let mut f = BufWriter::new(File::create(&dest_path)?);
 
-    let long_string = "abc".repeat(100);
-    write!(f, "{}", long_string)?;
+    let compile_time = chrono::Utc::now().to_rfc3339();
+    let value = "Hello, world!";
+    let value_hash = sha2::Sha256::digest(value.as_bytes());
+
+    let key_data = KeyData {
+        value,
+        value_hash: hex::encode(value_hash),
+        compile_time,
+    };
+
+    let json_data = serde_json::to_string(&key_data)?;
+    write!(f, "{}", json_data.to_string())?;
 
     Ok(())
 }
--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,5 +1,34 @@
-static LONG_STRING: &'static str = include_str!(concat!(env!("OUT_DIR"), "/long_string.txt"));
+use serde::{Deserialize, Serialize};
+use sha2::Digest;
+
+// Shared between build.rs and main.rs
+#[derive(Serialize, Deserialize, Debug)]
+struct KeyData<'a> {
+    value: &'a str,
+    value_hash: String,
+    compile_time: String,
+}
+
+static KEY: &'static str = include_str!(concat!(env!("OUT_DIR"), "/key.json"));
 
 fn main() {
-    println!("This package was compiled at {}", LONG_STRING);
+    let key_data: KeyData = serde_json::from_str(KEY).unwrap();
+
+    // Print the key data
+    let args: Vec<String> = std::env::args().collect();
+    if args.contains(&"--help".to_string()) {
+        println!("Usage: {} [--json] [--help]", args[0]);
+        println!("--json: Print the key data as JSON");
+        println!("--help: Print this help message");
+        return;
+    } else if args.contains(&"--json".to_string()) {
+        println!("{}", serde_json::to_string_pretty(&key_data).unwrap());
+        return;
+    }
+
+    // Check the hash of the value
+    let value_hash = sha2::Sha256::digest(key_data.value.as_bytes());
+    let hash_match = hex::encode(value_hash) == key_data.value_hash;
+
+    println!("Hash match: {}", hash_match);
 }
