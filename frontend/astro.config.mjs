2eb8f2ee306497a2396d8631daa3be0a96e9b412
Xevion <xevion@xevion.dev>
2024-12-22 02:22:24 -0600

    Teardown astrojs base, use tailwindcss, preact, sitemap

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), preact()]
+});
