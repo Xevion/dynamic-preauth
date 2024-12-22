a0417e0b1989abec69634d311df83e36e299e29a
Xevion <xevion@xevion.dev>
2024-12-22 03:08:44 -0600

    Set proper site URL, preact devtools, sitemap, robots.txt

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
-
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), preact()]
+  site: process.env.DEV
+    ? "https://localhost:4321"
+    : `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`,
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "monthly",
+      priority: 1.0,
+      // xslURL: "/sitemap.xsl",
+    }),
+    preact({
+      devtools: process.env.DEV != undefined ? true : false,
+    }),
+  ],
 });
