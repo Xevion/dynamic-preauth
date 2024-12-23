152635d7dbb418e4011dc8b082f76e9795acab38
Xevion <xevion@xevion.dev>
2024-12-23 13:35:56 -0600

    platform detection type + func

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -9,3 +9,12 @@ export function cn(...inputs: ClassValue[]) {
 export function plural(text: string, count: number) {
   return `${text}${count === 1 ? "" : "s"}`;
 }
+
+export type Platform = "windows" | "mac" | "linux";
+
+export function os(): Platform | "other" {
+  if (navigator.userAgent.includes("win")) return "windows";
+  else if (navigator.userAgent.includes("mac")) return "mac";
+  else if (navigator.userAgent.includes("linux")) return "linux";
+  return "other";
+}
