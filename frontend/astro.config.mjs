cdbc47481954cca6c08fbf762e73812d51e05ff1
Xevion <xevion@xevion.dev>
2025-01-02 16:47:30 -0600

    Pass ARG RAILWAY_PUBLIC_DOMAIN

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
@@ -7,22 +7,21 @@ import react from "@astrojs/react";
 
 // TODO: Add linting to build steps
 
-console.log(import.meta.env);
 // https://astro.build/config
 
 export default defineConfig({
   build: {
     assets: "assets",
   },
-  site: import.meta.env.DEV
-    ? "https://localhost:4321"
-    : // @ts-ignore
-      `https://${
-        import.meta.env.RAILWAY_PUBLIC_DOMAIN ??
-        (() => {
-          throw new Error("nullish");
-        })()
-      }`,
+  site:
+    process.env.NODE_ENV === "development"
+      ? "https://localhost:4321"
+      : `https://${
+          process.env.RAILWAY_PUBLIC_DOMAIN ??
+          (() => {
+            throw new Error("RAILWAY_PUBLIC_DOMAIN not set");
+          })()
+        }`,
   integrations: [
     tailwind(),
     sitemap({
