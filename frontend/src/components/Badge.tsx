d71433fa522eab650960a0d1fe260b6ba9869dff
Xevion <xevion@xevion.dev>
2025-01-02 14:54:56 -0600

    Fix minor react/badge issues

--- frontend/src/components/Badge.tsx
+++ frontend/src/components/Badge.tsx
@@ -18,7 +18,7 @@ const Badge = ({
     <span
       id="badge-dismiss-dark"
       className={cn(
-        "inline-flex align-middle items-center px-2 py-1 me-2 text-sm leading-none font-medium text-zinc-800 bg-zinc-100 rounded dark:bg-zinc-700 dark:text-zinc-300",
+        "inline-flex align-middle items-center px-2 py-2 text-sm leading-none font-medium rounded bg-zinc-700 text-zinc-300",
         className
       )}
     >
@@ -26,8 +26,7 @@ const Badge = ({
       <button
         type="button"
         onClick={onClick}
-        className="inline-flex items-center ms-1 text-sm text-zinc-400 bg-transparent rounded-sm hover:bg-zinc-200 hover:text-zinc-900 dark:hover:bg-zinc-600 dark:hover:text-zinc-300"
-        data-dismiss-target="#badge-dismiss-dark"
+        className="inline-flex zitems-center ms-1 text-sm text-zinc-400 bg-transparent rounded-sm hover:bg-zinc-600 hover:text-zinc-300"
         aria-label="Remove"
       >
         <svg
@@ -38,9 +37,9 @@ const Badge = ({
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
           />
         </svg>
